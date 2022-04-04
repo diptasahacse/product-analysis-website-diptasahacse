2975423a2cfb7fefd38291233d2bfec3292b7a64
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-04 16:42:41 +0600

    make necessary components like Header, Home, About, Dashboard, Reviews, Blogs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
+      <Home></Home>
       
     </div>
   );
