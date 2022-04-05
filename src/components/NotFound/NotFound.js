5d3a52c9f605aec65172c909dc4c7bf86bf42b58
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-05 20:49:39 +0600

    design 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import './NotFound.css'
+import animatedImg from '../../images/notfound.gif'
 const NotFound = () => {
     return (
-        <div className='text-center'>
-            <h2>Page Not Found</h2>
-            
+        <div className='container'>
+            <div className="row m-0 g-4 pt-5">
+                <div className="col-lg-7">
+                    <div>
+                        <h2 className='not-fount-hero-text'>Oops!</h2>
+                        <p  className='not-fount-des-text'>We can't seem to find the page you are looking for.</p>
+
+                    </div>
+                </div>
+                <div className="col-lg-5">
+                    <div>
+                        <img className='not-found-img' src={animatedImg} alt="" />
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
